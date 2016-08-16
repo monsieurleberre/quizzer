41dbce74968f16513d8865aa81fe7e8fcde70c26
monsieurleberre <monsieur.leberre@gmail.com>
2016-08-16 06:56:53 +0100

    fixed testSetup.js to make tests understand regenerators

--- tools/testSetup.js
+++ tools/testSetup.js
@@ -17,6 +17,30 @@ process.env.NODE_ENV = 'test';
   require.extensions[ext] = () => null;
 });
 
+// Make sure that the tests still understand generator syntax
+require('babel-regenerator-runtime');
 // Register babel so that it will transpile ES6 to ES5
 // before our tests run.
-require('babel-register')();
+require('babel-register')({});
+
+
+// Configure JSDOM and set global variables
+// to simulate a browser environment for tests.
+var jsdom = require('jsdom').jsdom;
+
+var exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
+
+documentRef = document;  //eslint-disable-line no-undef
